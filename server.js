9099d2a51ea2e22695d0a06ad7e16735386748a4
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2021-02-10 20:11:08 -0700

    deploying

--- server.js
+++ server.js
@@ -15,8 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetdb", 
+{
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
